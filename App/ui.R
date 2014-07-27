fa768a761e6ebe8e1fc922b4bf57e3d384f6fe58
Ahmed Shibrain <mr.shibrain@gmail.com>
2014-07-28 00:36:07 +0300

    Slides

--- App/ui.R
+++ App/ui.R
@@ -1,15 +1,28 @@
 library(shiny)
 shinyUI(pageWithSidebar(
-  headerPanel("MPG change factor inputs"),
+  headerPanel(
+    "MPG change factor inputs",p("asasa")
+    
+    ),
+
+
   sidebarPanel(
+    p("You can change the variables: Transmission Type, Gross Hourse Power, Wieght for a car"),
+    
     
     selectInput("tm", "Transmission Type",
                 choices = c("Automatic Transmission" = "automatic",
                          "Manual Transmission" = "manual")),
+    p("\n Now Select the gross Hourse Power of the car"),
     numericInput('hp', 'Hourse Power', 50, min = 50, max = 350, step = 50),
-    numericInput('wt', 'Wieght', 1000, min = 1000, max = 6000, step = 500)
+    p("The wight of the car in lb"),
+    numericInput('wt', 'Wieght', 1000, min = 1000, max = 6000, step = 500),
+    p("\n\n Notice that options are limited by the data used to fit the prediction model")
   ),
   mainPanel(
+    p("The Outpot is a prediction made by linear regression model built from mtcars data set"),
+    p("mtcars is data extracted from the 1974 Motor Trend US magazine, and comprises fuel consumption and 10 aspects of automobile design and performance for 32 automobiles (1973-74 models).."),
+    
     h3('prediction'),
     verbatimTextOutput("prediction"),h4('mile per US galon')
   )
